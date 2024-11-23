c1893427302b15b93e3b5e51abf84ed68c0dacfa
rduo1009 <robertduong09alt@gmail.com>
2024-11-23 20:36:32 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ IGNORE_MODULE=$([[ "$4" == true ]] && echo '-M' || echo '')
 VERBOSE=$([[ "$5" == true ]] && echo '--verbose' || echo '')
 
 if interrogate --fail-under $2 --generate-badge $3 $1 | grep -q 'PASSED'; then
-    interrogate --fail-under $2 --generate-badge $3 $IGNORE_MODULE $1
+    interrogate --fail-under $2 --generate-badge $3 $IGNORE_MODULE $1 -c setup.cfg
     exit 0
 else 
-    interrogate --fail-under $2 --generate-badge $3 $IGNORE_MODULE $1 $VERBOSE
+    interrogate --fail-under $2 --generate-badge $3 $IGNORE_MODULE $1 $VERBOSE -c setup.cfg
     exit 1
 fi
